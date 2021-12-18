c134ee0babbfb19f0560df79fadc46e651f580b3
dolverson <olverson@protonmail.com>
2021-12-18 21:14:50 +0000

    Creating an EC2 instance

---
+++ EC2/variables.tf
@@ -0,0 +1,10 @@
+variable "access_key" {
+  description = "AWS access"
+  type        = string
+  sensitive   = true
+}
+variable "secret_key" {
+  description = "AWS secret"
+  type        = string
+  sensitive   = true
+}
