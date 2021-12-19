36e8a108ec25f5b6b0befc8360bf4380dc3a7e26
dolverson <olverson@protonmail.com>
2021-12-19 02:07:05 +0000

    Try again

--- EC2/main.tf
+++ EC2/main.tf
@@ -1,5 +1,78 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = ""
-    secret_key = ""
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_security_group" "ubuntu" {
+  name        = "ubuntu-security-group"
+  description = "Allow HTTP, HTTPS and SSH traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+
+resource "aws_instance" "ubuntu" {
+  key_name      = "Proton Kube Key"
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ubuntu"
+  }
+
+  vpc_security_group_ids = [
+    aws_security_group.ubuntu.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("key")
+    host        = self.public_ip
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_type = "gp2"
+    volume_size = 30
+  }
+}
+
+resource "aws_eip" "ubuntu" {
+  vpc      = true
+  instance = aws_instance.ubuntu.id
 }
