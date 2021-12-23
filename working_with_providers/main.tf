d4ac39a641f188b52a3bc784f4de323ae9a46943
dolverson <olverson@protonmail.com>
2021-12-23 13:17:30 +0000

    Learning about providers and versioning

---
+++ working_with_providers/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+  acl    = "public-read"
+
+  region = "us-east-1"
+}
